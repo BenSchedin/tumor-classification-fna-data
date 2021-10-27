b78a738baeaa29cabbdcd5b483b91a478b6ed5be
Ben Schedin <49740457+BenSchedin@users.noreply.github.com>
2021-10-26 23:28:55 -0700

    added build_model()

--- project-files/modeling.py
+++ project-files/modeling.py
@@ -10,31 +10,23 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.neural_network import MLPClassifier
-from sklearn.metrics import accuracy_score
 
 
 def main():
     # Loading preprocessed data
     full = pd.read_csv("../data/processed/full.csv", header=None)
     scaled = pd.read_csv("../data/processed/full-scaled.csv", header=None)
-    pca = pd.read_csv("../data/processed/full-scaled.csv", header=None)
+    pca = pd.read_csv("../data/processed/full-pca.csv", header=None)
     pca7 = pd.read_csv("../data/processed/pca7.csv", header=None)
 
     # Prototyping code with MLP
-    # data prep
-    train_data, test_data, train_labels, test_labels = prep_data(scaled, 0.25)
+    accuracy = build_model(pca, 0.20, MLPClassifier, 1000)
 
-    # model implementation
-    max_epochs = 1000
-    mlp = MLPClassifier(max_iter=max_epochs, random_state=123, verbose=1)
-    mlp.fit(train_data, train_labels)
-    test_accuracy = round(mlp.score(test_data, test_labels) * 100, 2)
-
-    print(f"final accuracy: {test_accuracy}%")
+    print(f"final accuracy: {accuracy}%")
 
 
 def prep_data(data, test_percent):
-    """ Takes a preprocessed data file and creates train and test sets for sk-learn """
+    """ Takes a preprocessed data file and creates train and test sets for sklearn """
 
     # Splitting labels from the data
     labels = data[0]
@@ -45,6 +37,16 @@ def prep_data(data, test_percent):
 
     # Returning prepared data
     return train_data, test_data, train_labels, test_labels
+
+
+def build_model(data, test_percent, algorithm, max_epochs):
+    train_data, test_data, train_labels, test_labels = prep_data(data, test_percent)
+
+    model = algorithm(max_iter=max_epochs, random_state=123, verbose=1)
+    model.fit(train_data, train_labels)
+    test_accuracy = round(model.score(test_data, test_labels) * 100, 2)
+
+    return test_accuracy
 
 
 if __name__ == "__main__":
