56bfc1e23fdfc4330c53dfe32835b0e988821a1a
Ben Schedin <49740457+BenSchedin@users.noreply.github.com>
2021-10-26 23:10:46 -0700

    added prep_data to modeling.py

--- project-files/modeling.py
+++ project-files/modeling.py
@@ -9,6 +9,8 @@
 
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn.neural_network import MLPClassifier
+from sklearn.metrics import accuracy_score
 
 
 def main():
@@ -18,8 +20,17 @@ def main():
     pca = pd.read_csv("../data/processed/full-scaled.csv", header=None)
     pca7 = pd.read_csv("../data/processed/pca7.csv", header=None)
 
-    # Splitting the class labels from the data
-    print(len(full.columns))
+    # Prototyping code with MLP
+    # data prep
+    train_data, test_data, train_labels, test_labels = prep_data(scaled, 0.25)
+
+    # model implementation
+    max_epochs = 1000
+    mlp = MLPClassifier(max_iter=max_epochs, random_state=123, verbose=1)
+    mlp.fit(train_data, train_labels)
+    test_accuracy = round(mlp.score(test_data, test_labels) * 100, 2)
+
+    print(f"final accuracy: {test_accuracy}%")
 
 
 def prep_data(data, test_percent):
@@ -30,6 +41,10 @@ def prep_data(data, test_percent):
     data = data.iloc[:, 1:len(data.columns)]
 
     # Splitting data into train and test sets
+    train_data, test_data, train_labels, test_labels = train_test_split(data, labels, test_size=test_percent, random_state=123)
+
+    # Returning prepared data
+    return train_data, test_data, train_labels, test_labels
 
 
 if __name__ == "__main__":
